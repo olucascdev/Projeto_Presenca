fc16ba333386152e6061e46b0dbfa98d287c94c7
Lucas Correia <lucasdevproject@gmail.com>
2024-11-29 08:45:18 -0300

    .

--- Controller/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'config.php';
-
-
-if ($conn->connect_errno) {
-
-  echo "Falha na conexão: " . $conn->connect_error;
-  exit();
-}
--- Controller/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$server = 'localhost';
-$user = 'root';
-$password = '';
-$db = 'projeto_presenca';
-
-$conn = mysqli_connect($server, $user, $password, $db);
